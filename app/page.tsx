5ee94f9610346742be0e702f9f9e05713c949c3c
Juanmorales1810 <juan1810.jm@gmail.com>
2025-03-13 12:07:12 -0300

    Agregar componente WhyUs a la página principal

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/interface/Hero";
+import WhyUs from "@/components/interface/WhyUs";
 import ProductsHover from "@/components/ProductsHover/productsHover";
 
 export default function Home() {
     return (
         <main className="flex flex-col justify-center items-center w-full">
             <Hero />
+            <WhyUs />
             <ProductsHover />
         </main>
     );
