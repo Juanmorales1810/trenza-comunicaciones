9d4986dce893e34b5a3bb1f0a2f0eb523df011ca
Juanmorales1810 <juan1810.jm@gmail.com>
2025-03-14 12:27:33 -0300

    Agregar componente Footer con enlaces de navegación y redes sociales

---
+++ components/interface/Footer.tsx
@@ -0,0 +1,104 @@
+import { Instagram, Linkedin, Twitter } from "lucide-react";
+import React from "react";
+import TextPressure from "../text-pressure";
+import Image from "next/image";
+
+export default function Footer() {
+    return (
+        <section className="">
+            <div className="w-full px-24 flex justify-between gap-8 border-x border-t py-4 max-md:flex-col lg:py-8">
+                <div className="mb-8 flex-1">
+                    <a href="/" className="flex items-center gap-2">
+                        <Image
+                            src="/image/logoTrenza.png"
+                            alt="logo"
+                            width="129"
+                            height="32"
+                            className="dark:invert"
+                        />
+                    </a>
+                </div>
+                <div className="flex flex-1 justify-between gap-8 max-sm:flex-col">
+                    <div>
+                        <h3 className="text-muted-foreground-subtle text-sm font-semibold tracking-[-0.28px]">
+                            Product
+                        </h3>
+                        <ul className="mt-6 space-y-6 text-sm tracking-[-0.28px] lg:mt-8 lg:space-y-8">
+                            <li className="hover:text-primary">
+                                <a href="#">Features</a>
+                            </li>
+                            <li className="hover:text-primary">
+                                <a href="#">Pricing</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h3 className="text-muted-foreground-subtle text-sm font-semibold tracking-[-0.28px]">
+                            Company
+                        </h3>
+                        <ul className="mt-6 space-y-6 text-sm tracking-[-0.28px] lg:mt-8 lg:space-y-8">
+                            <li className="hover:text-primary">
+                                <a href="/contact">Contact</a>
+                            </li>
+                            <li className="hover:text-primary">
+                                <a href="#">Faq</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h3 className="text-muted-foreground-subtle text-sm font-semibold tracking-[-0.28px]">
+                            Legal
+                        </h3>
+                        <ul className="mt-6 space-y-6 text-sm tracking-[-0.28px] lg:mt-8 lg:space-y-8">
+                            <li className="hover:text-primary">
+                                <a href="/terms-of-service">Terms of Service</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h3 className="text-muted-foreground-subtle text-sm font-semibold tracking-[-0.28px]">
+                            Social
+                        </h3>
+                        <div className="text-muted-foreground-subtle mt-6 flex gap-3 lg:mt-8">
+                            <a
+                                href="https://instagram.com"
+                                aria-label="Instagram"
+                            >
+                                <Instagram className="size-5" />
+                            </a>
+                            <a href="https://twitter.com" aria-label="Twitter">
+                                <Twitter className="size-5" />
+                            </a>
+                            <a
+                                href="https://Linkedin.com"
+                                aria-label="Linkedin"
+                            >
+                                <Linkedin className="size-5" />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="text-muted-foreground-subtle w-full px-24 border-x border-t border-b py-4 text-sm tracking-[-0.28px] lg:py-8">
+                <p>
+                    © {new Date().getFullYear()} Trenza Comunicaciones. All
+                    rights reserved.
+                </p>
+            </div>
+            <div className="w-full px-24 border-x hidden md:block">
+                <TextPressure
+                    text="Trenza"
+                    flex={true}
+                    alpha={true}
+                    stroke={false}
+                    width={true}
+                    weight={true}
+                    italic={true}
+                    textColor="#ffffff"
+                    strokeColor="#ff0000"
+                    minFontSize={32}
+                />
+            </div>
+        </section>
+    );
+}
