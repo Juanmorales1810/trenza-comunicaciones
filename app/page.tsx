3a215d2c9487df8802e14be238cad262a93fb2fb
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2025-03-13 22:53:06 -0300

    Comentar el componente ProductsHover en la página principal

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Hero from "@/components/interface/Hero";
 import Service from "@/components/interface/Service";
 import Team from "@/components/interface/Team";
 import WhyUs from "@/components/interface/WhyUs";
-import ProductsHover from "@/components/ProductsHover/productsHover";
 
 export default function Home() {
     return (
@@ -10,7 +9,6 @@ export default function Home() {
             <Hero />
             <WhyUs />
             <Service />
-            {/* <ProductsHover /> */}
             <Team />
         </main>
     );
