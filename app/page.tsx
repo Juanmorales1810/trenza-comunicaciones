1ebd3289b2a7918a0a973a694afcb06b6f592772
Juanmorales1810 <juan1810.jm@gmail.com>
2025-03-14 12:26:48 -0300

    Agregar componentes Testimonial, Contact y Cta a la página principal

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import Contact from "@/components/interface/Contact";
+import Cta from "@/components/interface/Cta";
 import Hero from "@/components/interface/Hero";
 import Service from "@/components/interface/Service";
 import Team from "@/components/interface/Team";
+import Testimonial from "@/components/interface/Testimonial";
 import WhyUs from "@/components/interface/WhyUs";
 
 export default function Home() {
@@ -9,7 +12,10 @@ export default function Home() {
             <Hero />
             <WhyUs />
             <Service />
+            <Testimonial />
             <Team />
+            <Contact />
+            <Cta />
         </main>
     );
 }
